8058724701ec4e5ee5eb6fd249d5f5d67db2057a
rajan88gupta <104194078+rajan88gupta@users.noreply.github.com>
2026-02-10 21:13:29 +1300

    Create main.tf

---
+++ 04-terraform-iac/main.tf
@@ -0,0 +1,25 @@
+resource "aws_vpc" "this" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "${var.project_name}-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.project_name}-public-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
+}
